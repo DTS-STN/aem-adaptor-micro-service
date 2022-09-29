3def7e53f2c5e7bb516511c5d6981ddd714fdf0a
L Harrison <lhr.git@gmail.com>
2022-09-29 16:10:59 -0400

    Sorted/formatted files, added in middleware cache

--- app/lib/mapper/sch_mapper.py
+++ app/lib/mapper/sch_mapper.py
@@ -1,4 +1,2 @@
 def map_home(aem_content):
-    return {
-        "title": aem_content["alphaDCHomeByPath"]["item"]["scTitleEn"]
-    }
+    return {"title": aem_content["alphaDCHomeByPath"]["item"]["scTitleEn"]}
--- app/main.py
+++ app/main.py
@@ -1,10 +1,52 @@
+import json
+import os
 from typing import Any, Dict, Union
 
+import redis
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.concurrency import iterate_in_threadpool
+from starlette.responses import JSONResponse, StreamingResponse
+
 from app.routers import sch_router
 
 app = FastAPI()
+
+
+@app.middleware("http")
+async def handle_cache(request: Request, call_next: Any) -> Union[Any, None]:
+    # read in env var for redis host, port, and db
+    path = request.url.path.replace("/", "_")
+
+    # establish redis connection
+    r = redis.Redis(
+        host=os.getenv("REDIS_HOST"),
+        port=os.getenv("REDIS_PORT"),
+        db=os.getenv("REDIS_DB"),
+        username=os.getenv("REDIS_USERNAME"),
+        password=os.getenv("REDIS_PASSWORD"),
+    )
+
+    # check redis for values
+    if r.exists(path):
+        # if a key is found, return the value
+        return JSONResponse(json.loads(r.get(path)))
+
+    response = await call_next(request)
+
+    # if no key is found, and the response is good then the request proceeds
+    if response.status_code == 200:
+        response_body = [chunk async for chunk in response.body_iterator]
+        response.body_iterator = iterate_in_threadpool(iter(response_body))
+        response_body2 = (b"".join(response_body)).decode()
+        json_decoded = json.loads(response_body2)
+
+        r.set(path, json.dumps(json_decoded))
+
+        return JSONResponse(json_decoded)
+
+    # if the value is not cached, and the value is not status 200, then just return the response
+    return response
 
 
 @app.get("/")
