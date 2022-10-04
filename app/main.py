99dcffe6074e09782ac47f7877a3299137ce0e47
L Harrison <lhr.git@gmail.com>
2022-10-04 14:21:44 -0400

    renamed some variables, added in better typechecking

--- app/main.py
+++ app/main.py
@@ -15,34 +15,55 @@ app = FastAPI()
 
 @app.middleware("http")
 async def handle_cache(request: Request, call_next: Any) -> Union[Any, None]:
-    # read in env var for redis host, port, and db
-    path = request.url.path.replace("/", "_")
+    # establish naming convention for cache keys, this will likely need to be revised to include request.url.query
+    cache_key = "home" + request.url.path.rstrip("/").replace("/", "_")
 
-    # establish redis connection
-    r = redis.StrictRedis(
+    # Create a redis connection pool, by reading in env vars
+    pool = redis.ConnectionPool(
         host=os.getenv("REDIS_HOST", "localhost"),
         port=int(os.getenv("REDIS_PORT", "6379")),
         db=int(os.getenv("REDIS_DB", "0")),
         username=os.getenv("REDIS_USER"),
         password=os.getenv("REDIS_PASSWORD"),
+        decode_responses=True,
     )
 
-    # check redis for values
-    if r.exists(path) and path != "":
-        # if a key is found, return the value
-        return JSONResponse(json.loads(r.get(path)))
+    # establish redis connection
+    r = redis.Redis(connection_pool=pool)
 
+    # check redis for values
+    if r.exists(cache_key):
+        # if a key is found, return the value
+        val = r.get(cache_key)
+        if val is not None:
+            return JSONResponse(
+                content=json.loads(val),
+                status_code=200,
+            )
+
+    # if no key is found, continue on with the request and await the response
     response = await call_next(request)
 
-    # if no key is found, and the response is good then the request proceeds
+    # if the response is successful, then place the json value into the cache
     if response.status_code == 200:
-        response_body = [chunk async for chunk in response.body_iterator]
-        response.body_iterator = iterate_in_threadpool(iter(response_body))
-        json_decoded = json.loads((b"".join(response_body)).decode())
+        if type(response) is StreamingResponse:
+            response_body = [chunk async for chunk in response.body_iterator]
+            response.body_iterator = iterate_in_threadpool(iter(response_body))
 
-        r.set(path, json.dumps(json_decoded))
+            # the response SHOULD be in bytes, but if it's not it'd be in a string
+            # you can't assert an instance of a list, we could write our own class,
+            # or we could do what I've done here, and ensured that the types match
+            # their join methods. Mypy doesn't like that, but it is typesafe
+            if all(isinstance(elements, bytes) for elements in response_body):
+                json_val = json.loads((b"".join(response_body)).decode())  # type: ignore
+            elif all(isinstance(elements, str) for elements in response_body):
+                json_val = json.loads(("".join(response_body)).decode())  # type: ignore
 
-        return JSONResponse(json_decoded)
+        if type(response) is JSONResponse:
+            json_val = response.body
+
+        # place value in the cache, after combining it
+        r.set(cache_key, json.dumps(json_val))
 
     # if the value is not cached, and the value is not status 200, then just return the response
     return response
@@ -57,4 +78,9 @@ app.include_router(sch_router.router)
 
 
 def start() -> None:
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host=os.getenv("LOCAL_HOST", "127.0.0.1"),
+        port=int(os.getenv("LOCAL_PORT", 8000)),
+        reload=bool(os.getenv("LOCAL_RELOAD", True)),
+    )
